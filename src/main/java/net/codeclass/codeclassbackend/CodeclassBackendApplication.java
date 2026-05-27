dfce509057aa7cbabfee45a37fa3a1897c4d6bf5
hyotatoFrappuccino <hjkiln0@gmail.com>
2026-05-27 15:28:15 +0900

    init commit

---
+++ src/main/java/net/codeclass/codeclassbackend/CodeclassBackendApplication.java
@@ -0,0 +1,13 @@
+package net.codeclass.codeclassbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeclassBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeclassBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/codeclass/codeclassbackend/CodeclassBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package net.codeclass.codeclassbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeclassBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
